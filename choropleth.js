c67ea34ff97c68e8a232f08f6da3408e672d5e34
Jonathan <jhday001@louisville.edu>
2014-04-07 13:24:04 -0400

    Mouseover changes

--- choropleth.js
+++ choropleth.js
@@ -38,9 +38,25 @@ svg.call(tip);
 // Grab the geo data
 queue()
   .defer(d3.json, "us.json")
-	.defer(d3.csv, "data.csv", function(d) {map.set(d.STATE, +d.AMOUNT); })
+	.defer(d3.csv, "usData.csv", function(d) {map.set(StateToFips(d.STATE), +d.AMOUNT); })
   .await(update);
-  
+	
+var myMouseOverFunction = function() {
+	var state = d3.select(this);
+	state.attr("fill", "red" );
+	 
+	// Show tooltip
+	state.on('mouseover', tip.show);
+};
+
+var myMouseOutFunction = function() {
+	var state = d3.select(this);
+	state.attr("fill", "none" );
+	
+	// Hide tooltip
+	state.on('mouseoout', tip.hide);
+};
+
 function update(err, us) {
   console.log(err);
   console.log(us);
@@ -51,8 +67,79 @@ function update(err, us) {
       .data(topojson.feature(us, us.objects.states).features)
       .enter().append("path")
         .attr("d", path)
-		.on('mouseover', tip.show)
-    .on('mouseout', tip.hide)
+			.on('mouseover', myMouseOverFunction)
+			.on('mouseout', myMouseOutFunction);
 };
 
 d3.select(self.frameElement).style("height", height + "px");
+
+// Define a lookup table for the FIPS codes
+var fips = {
+	"AL": "01",
+	"AK": "02",
+	"AS": "60",
+	"AZ": "04",
+	"AR": "05",
+	"CA": "06",
+	"CO": "08",
+	"CT": "09",
+	"DE": "10",
+	"DC": "11",
+	"FL": "12",
+	"FM": "64",
+	"GA": "13",
+	"GU": "66",
+	"HI": "15",
+	"ID": "16",
+	"IL": "17",
+	"IN": "18",
+	"IA": "19",
+	"KS": "20",
+	"KY": "21",
+	"LA": "22",
+	"ME": "23",
+	"MH": "68",
+	"MD": "24",
+	"MA": "25",
+	"MI": "26",
+	"MN": "27",
+	"MS": "28",
+	"MO": "29",
+	"MT": "30",
+	"NE": "31",
+	"NV": "32",
+	"NH": "33",
+	"NJ": "34",
+	"NM": "35",
+	"NY": "36",
+	"NC": "37",
+	"ND": "38",
+	"MP": "69",
+	"OH": "39",
+	"OK": "40",
+	"OR": "41",
+	"PW": "70",
+	"PA": "42",
+	"PR": "72",
+	"RI": "44",
+	"SC": "45",
+	"SD": "46",
+	"TN": "47",
+	"TX": "48",
+	"UM": "74",
+	"UT": "49",
+	"VT": "50",
+	"VA": "51",
+	"VI": "78",
+	"WA": "53",
+	"WV": "54",
+	"WI": "55",
+	"WY": "56"
+};
+ 
+// Function to use the lookup table to do the conversion
+// sc argument is a string containing the two char
+// state abbreviation
+var StateToFips = function(sc) {
+	return fips[sc];
+};
